817cb605155279b3e9f8c1f6092962e1a67062e2
Sualeh Farooq <sualehfarooq65@gmail.com>
2024-05-12 19:02:04 +0500

    protect api keys with env

--- src/pages/api/getorders.js
+++ src/pages/api/getorders.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://adjkbaqvoxmzorgmrees.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkamtiYXF2b3htem9yZ21yZWVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NzgxNTIsImV4cCI6MjAyNjM1NDE1Mn0.HXDGP9uxjGMsh5IrO5w12HHqxS31yTHUsKGOhKlcHZM';
+const supabaseUrl = process.env.NEXT_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
