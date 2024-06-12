a6b6c5de9ec8dde80ce5d3df722f96164beeee7d
Sualeh Farooq <72302978+sualeh-farooq@users.noreply.github.com>
2024-06-12 20:34:54 +0500

    Update server_config.js

--- server_config.js
+++ server_config.js
@@ -1,5 +1,5 @@
-  const AppUrl = "http://localhost:3000"
+  //const AppUrl = "http://localhost:3000"
   // const AppUrl = "https://n-healthcare-updated.vercel.app"
-  // const AppUrl = "https://nhealthcare.com.pk/"
+  const AppUrl = "https://nhealthcare.com.pk/"
 
   export default AppUrl;
